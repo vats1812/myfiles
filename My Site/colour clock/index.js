19e9cf4b268021aeda8f45365c6ffd3d2535c485
vats1812 <68377043+vats1812@users.noreply.github.com>
2020-08-30 10:52:38 +0530

    Add files via upload

---
+++ My Site/colour clock/index.js
@@ -0,0 +1,46 @@
+let hours = document.getElementById("hours");
+let minutes = document.getElementById("minutes");
+let seconds = document.getElementById("seconds");
+let clock = () => {
+
+    hours.innerHTML = new Date().getHours();
+    minutes.innerHTML = new Date().getMinutes();
+    seconds.innerHTML = new Date().getSeconds();
+
+    if (hours.innerHTML.length == 1) {
+        hours.innerHTML = "0" + hours.innerHTML;
+    }
+
+    if (minutes.innerHTML.length == 1) {
+        minutes.innerHTML = "0" + minutes.innerHTML;
+    }
+    if (seconds.innerHTML.length == 1) {
+        seconds.innerHTML = "0" + seconds.innerHTML;
+    }
+
+}
+
+setInterval(() => {
+    clock()
+}, 1000);
+// alternate method for time
+// let interval = setInterval(clock, 1000);
+
+
+let picker = document.getElementById("picker");
+picker.addEventListener('input', colours = () => {
+    let rang = picker.value;
+
+    document.getElementById("hours").style.color = rang;
+    document.getElementById("minutes").style.color = rang;
+    document.getElementById("seconds").style.color = rang;
+});
+
+let backpicker = document.getElementById("backpicker");
+backpicker.addEventListener('input', backcolours = () => {
+    let rang = backpicker.value;
+
+    document.getElementById("hours").style.background = rang;
+    document.getElementById("minutes").style.background = rang;
+    document.getElementById("seconds").style.background = rang;
+});
